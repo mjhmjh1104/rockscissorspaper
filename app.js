4ab88ed063f9a8c6529c53febe8ecae0efb19daf
mjhmjh1104 <mjhmjh1104@naver.com>
2018-05-06 16:11:58 +0900

    changed

--- app.js
+++ app.js
@@ -18,9 +18,10 @@ var dataSchema = mongoose.Schema({
     Main: String,
     Challenger: String,
     Waiting: Number,
-    Challenge: [Number],
     StartMain: Number,
-    StartChallenger: Number
+    StartChallenger: Number,
+    MainGame: Number,
+    ChallengerGame: Number
   }],
   RoomCount: Number
 });
@@ -58,7 +59,7 @@ app.get('/create', function(req, res) {
   Data.findOne({Name: 'main'}, function(err, data) {
     if (err) return console.log('DATA ERROR\n', err);
     var length = data.Property.length;
-    data.Property.push({Num: data.RoomCount++, Main: req.query.id, Waiting: 1, StartMain: 0, StartChallenger: 0});
+    data.Property.push({Num: data.RoomCount++, Main: req.query.id, Waiting: 1, StartMain: 0, StartChallenger: 0, MainGame: 0, ChallengerGame: 0});
     data.save(function(err){
       if (err) return console.log('DATA ERROR\n', err);
     });
@@ -145,6 +146,47 @@ app.get('/start', function(req, res) {
         if (err) return console.log('DATA ERROR\n', err);
       });
       number.num =  data.Property[i].StartMain;
+      res.render('Show', number);
+    }
+  });
+});
+
+app.get('/gamestart', function(req, res) {
+  Data.findOne({Name: 'main'}, function(err, data) {
+    if (err) return console.log('DATA ERROR\n', err);
+    for (var i = 0; i < data.Property.length; i++)
+      if (data.Property[i].Num == req.query.room) break;
+    data.Property[i].StartMain = 0;
+    data.Property[i].MainGame = 0;
+    data.Property[i].StartChallenger = 0;
+    data.Property[i].ChallengerGame = 0;
+    data.save(function(err) {
+      if (err) return console.log('DATA ERROR\n', err);
+    });
+    var number = {num: "done"};
+    res.render('Show', number);
+  });
+});
+
+app.get('/game', function(req, res) {
+  Data.findOne({Name: 'main'}, function(err, data) {
+    if (err) return console.log('DATA ERROR\n', err);
+    for (var i = 0; i < data.Property.length; i++)
+      if (data.Property[i].Num == req.query.room) break;
+    var number = {num: 0};
+    if (req.query.main == 1) {
+      data.Property[i].MainGame = req.query.game;
+      data.save(function(err) {
+        if (err) return console.log('DATA ERROR\n', err);
+      });
+      number.num = data.Property[i].ChallengerGame;
+      res.render('Show', number);
+    } else {
+      data.Property[i].ChallengerGame = req.query.game;
+      data.save(function(err) {
+        if (err) return console.log('DATA ERROR\n', err);
+      });
+      number.num = data.Property[i].MainGame;
       res.render('Show', number);
     }
   });
